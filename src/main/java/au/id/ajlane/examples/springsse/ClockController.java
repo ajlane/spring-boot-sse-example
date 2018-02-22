4d89034550a1368033153716fe40897d83fdae59
Aaron Lane <aaron.lane@d2dcrc.com.au>
2018-02-22 13:11:54 +1030

    Initial commit

---
+++ src/main/java/au/id/ajlane/examples/springsse/ClockController.java
@@ -0,0 +1,38 @@
+package au.id.ajlane.examples.springsse;
+
+import org.apache.catalina.connector.ClientAbortException;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@RestController
+public class ClockController {
+
+    private static final Collection<SseEmitter> emitters = new ConcurrentLinkedQueue<>();
+
+    @RequestMapping("/clock")
+    public SseEmitter subscribe() {
+        SseEmitter emitter = new SseEmitter();
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitters.add(emitter);
+        return emitter;
+    }
+
+    @Scheduled(fixedRate = 100)
+    public void tick() throws IOException {
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(Instant.now().toString(), MediaType.TEXT_PLAIN);
+            } catch (ClientAbortException ex){
+                emitter.complete();
+            }
+        }
+    }
+}
